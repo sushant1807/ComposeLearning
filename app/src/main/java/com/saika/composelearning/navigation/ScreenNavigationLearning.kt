d9a7524ff3242af8bf6769fa0c02ab73195efe52
Sushant <v867193@global.ual.com>
2025-02-21 14:46:37 -0600

    Fundamentals on Navigation with transitions

--- app/src/main/java/com/saika/composelearning/navigation/ScreenNavigationLearning.kt
+++ app/src/main/java/com/saika/composelearning/navigation/ScreenNavigationLearning.kt
@@ -1,5 +1,9 @@
 package com.saika.composelearning.navigation
 
+import androidx.compose.animation.AnimatedContentTransitionScope
+import androidx.compose.animation.AnimatedVisibilityScope
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.slideOut
 import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -11,12 +15,19 @@ fun ScreenNavigationLearning(){
     NavHost(navController = navController, startDestination = "screen_a") {
 
         composable(
-            route = "screen_a"
-        ) {
-
-        }
-        composable(
-            route = "screen_a"
+            route = "screen_a",
+            exitTransition = {
+                slideOutOfContainer(
+                    AnimatedContentTransitionScope.SlideDirection.Left,
+                    tween(1000)
+                )
+            },
+            popEnterTransition = {
+                slideIntoContainer(
+                    AnimatedContentTransitionScope.SlideDirection.Right,
+                    tween(1000)
+                )
+            }
         ) {
             ScreenA(
                 navigateToB = {
@@ -26,7 +37,19 @@ fun ScreenNavigationLearning(){
         }
 
         composable(
-            route = "screen_b"
+            route = "screen_b",
+            exitTransition = {
+                slideOutOfContainer(
+                    AnimatedContentTransitionScope.SlideDirection.Left,
+                    tween(1000)
+                )
+            },
+           popExitTransition  = {
+                slideOutOfContainer(
+                    AnimatedContentTransitionScope.SlideDirection.Left,
+                    tween(1000)
+                )
+            }
         ) {
             ScreenB(
                 navigateToC = {
@@ -39,15 +62,34 @@ fun ScreenNavigationLearning(){
         }
 
         composable(
-            route = "screen_c"
+            route = "screen_c",
+            exitTransition = {
+                slideOutOfContainer(
+                    AnimatedContentTransitionScope.SlideDirection.Left,
+                    tween(1000)
+                )
+            },
+            popExitTransition  = {
+                slideOutOfContainer(
+                    AnimatedContentTransitionScope.SlideDirection.Left,
+                    tween(1000)
+                )
+            }
         ) {
             ScreenC(
                 navigateToB = {
                     navController.popBackStack()
                 },
                 navigateToA = {
-                    navController.popBackStack()
-                    navController.popBackStack()
+                    //Two ways to navigate back to A
+                    navController.navigate("screen_a") {
+                        //This is for clearing the backstack
+                        popUpTo(0)
+                    }
+
+
+                    //navController.popBackStack()
+                    //navController.popBackStack()
                 }
             )
         }
